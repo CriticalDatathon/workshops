db8b305736a9764dee10a016484165d6a76a4b0b
João Matos <joaocarlosm00@gmail.com>
2023-05-06 20:23:23 +0100

    Add bypass script for workshop 1

--- solutions/workshop_1.py
+++ solutions/workshop_1.py
@@ -1,1 +1,8 @@
-import pandas as 
+import pandas as pd
+
+data = pd.read_csv("data/mimic_pulseOx_data.csv")
+data = data[(data.SaO2 <= 100) & (data.SpO2 <= 100) & (data.SaO2 >= 65) & (data.SpO2 >= 65)]
+data = data[data['delta_SpO2'] >= -10]
+
+data.to_csv("data/solutions/workshop_1.csv", index=False)
+
